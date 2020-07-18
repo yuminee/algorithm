aa9478914395b0e552a113ab21db67f4592eac04
youmin <33755241+yuminee@users.noreply.github.com>
2020-07-18 11:54:10 +0900

    Update the_largest_number.py

--- the_largest_number.py
+++ the_largest_number.py
@@ -1,38 +1,6 @@
+#주어진 리스트에서 가장 큰 수 찾기.
+#원소는 0이상 1000이하
 def solution(numbers):
-    a = len(numbers)
-    b = 0
-    for i in range(0,a):
-        for j in range(0,a):
-            if (numbers[i] < 10) and (10 <=numbers[j] < 100) :
-                if numbers[j]/10 <= numbers[i]:
-                    temp = numbers[j]
-                    numbers[j] = numbers[i]
-                    numbers[i] = temp
-                    if numbers[j]%10 > numbers[i]:
-                        temp = numbers[j]
-                        numbers[j] = numbers[i]
-                        numbers[i] = temp
-                    if (numbers[j] > numbers[i]) and (numbers[j]%10 ==0):
-                        temp = numbers[j]
-                        numbers[j] = numbers[i]
-                        numbers[i] = temp
-                if numbers[i] == 1000:
-                    temp = numbers[a-1]
-                    numbers[a-1] = numbers[i]
-                    numbers[i] = temp
-                if numbers[i] ==0:
-                    b = b + 1
-
-
-            
-    
-    
-    y = ''
-
-    for i in range(0,a):
-        x = str(numbers[i])
-        y = y+x
-    
-    for i in range(0,b):
-        y = y+'0'    
-    return y
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
