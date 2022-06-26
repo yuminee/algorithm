ef3f7a8612df5f3c53335eb0d5129b7575cbb614
yu min park <yumin@yuui-MacBookPro.local>
2022-06-26 17:55:31 +0900

    기존 파일 정리

--- PROGRAMMERS/the_largest_number.py
+++
@@ -1,6 +0,0 @@
-#주어진 리스트에서 가장 큰 수 찾기.
-#원소는 0이상 1000이하
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
