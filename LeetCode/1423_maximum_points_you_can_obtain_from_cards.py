961d60b8158c261ec25639d26658fa8c8a5dd40c
yu min park <ayuminee2@gmail.com>
2022-06-27 06:51:54 +0900

    1423: maximum points you can obtain from cards, Space Optimized Solution2

--- LeetCode/1423_maximum_points_you_can_obtain_from_cards.py
+++ LeetCode/1423_maximum_points_you_can_obtain_from_cards.py
@@ -1,6 +1,6 @@
 from typing import List
 
-class Solution:
+class Solution1:
     
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         total = sum(cardPoints[:k])
@@ -9,3 +9,22 @@ class Solution:
             total += cardPoints[len(cardPoints) - 1 - i] - cardPoints[k - 1 - i]
             max_total = max(max_total, total)
         return max_total
+
+class Solution2:
+    
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        front_score = sum(cardPoints[:k])
+        rear_score = 0
+        n = len(cardPoints)
+        
+        max_score = front_score
+        
+        for i in range(k-1, -1, -1):
+            rear_score += cardPoints[n - (k - i)]
+            front_score -= cardPoints[i]
+            current_score = rear_score + front_score
+            max_score = max(max_score, current_score)
+            
+        return max_score
+        
+        
