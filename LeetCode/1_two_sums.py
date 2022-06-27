201a14d993ea9b6c0f76b063ed3012941fd18af4
yu min park <ayuminee2@gmail.com>
2022-06-28 08:32:13 +0900

    1: two sums Solution2 using hash map

--- LeetCode/1_two_sums.py
+++ LeetCode/1_two_sums.py
@@ -6,3 +6,18 @@ class Solution:
             x = target - i
             if x in nums and nums.index(x) != idx:
                 return [idx, nums.index(x)]
+
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for idx, v in enumerate(nums):
+            complement = target - v
+            if complement in hashmap:
+                return [idx, hashmap[complement]]
+            
+            hashmap[v] = idx
+
+                
+            
+     
