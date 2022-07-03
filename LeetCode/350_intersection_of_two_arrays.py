e6b0d095e2b38eb4fa434c61ff8765e82492de4f
yu min park <ayuminee2@gmail.com>
2022-07-04 07:14:47 +0900

    350: Intersection of Two Arrays II 솔루션 추가

--- LeetCode/350_intersection_of_two_arrays.py
+++ LeetCode/350_intersection_of_two_arrays.py
@@ -4,7 +4,7 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         hashmap = {}
-        return_list = []
+        result = []
         for v1 in nums1:
             if v1 not in hashmap:
                 hashmap[v1] = 0
@@ -14,22 +14,28 @@ class Solution:
         for v2 in nums2:
             if v2 in hashmap:
                 if hashmap[v2] >= 1:
-                    return_list.append(v2)
+                    result.append(v2)
                     hashmap[v2] -=1
         
-        return return_list
+        return result
 
-                
-    
+class Solution2:
+    """
+    - 주어진 리스트들이 정렬되어 있는 경우
+    """
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-
-        
-            
+        result = []
+        i, j, k = 0, 0, 0
+        while (i < len(nums1) and j < len(nums2)):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+                k += 1
                 
-
-                    
-                
-                
-                
-             
-        
+        return result
