4cf3dd9f065407a680c346045b43fa03dd5c32e2
youmin <33755241+yuminee@users.noreply.github.com>
2023-10-10 19:55:14 +0900

    Time: 661 ms (96.3%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,9 @@
-select w1.id
-from Weather as w1
-cross join Weather as w2
-where DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+# select w1.id
+# from Weather as w1
+# cross join Weather as w2
+# where DATEDIFF(w1.recordDate, w2.recordDate) = 1 and w1.temperature > w2.temperature;
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature
