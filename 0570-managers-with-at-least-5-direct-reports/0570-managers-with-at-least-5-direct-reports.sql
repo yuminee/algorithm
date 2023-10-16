a8216139d515154916f250d0d325eb167ee27ad6
youmin <33755241+yuminee@users.noreply.github.com>
2023-10-16 22:28:14 +0900

    Time: 540 ms (73.52%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,17 @@
+# select name
+# from Employee as t1
+# inner join (
+#     select managerId
+#     from Employee
+#     group by managerId
+#     having count(managerId) >= 5
+# ) as t2 on t1.id = t2.managerId;
+
 select name
 from Employee as t1
-inner join (
+where id in (
     select managerId
     from Employee
     group by managerId
     having count(managerId) >= 5
-) as t2 on t1.id = t2.managerId;
+);
