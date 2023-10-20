7cc12d506094a6b159b1f48128035999a07c1fae
youmin <33755241+yuminee@users.noreply.github.com>
2023-10-20 23:02:07 +0900

    Time: 1245 ms (33.49%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -3,7 +3,7 @@ select
 DATE_FORMAT(trans_date,'%Y-%m') as month,
 country,
 count(country) as trans_count,
-sum(IF(state = "approved",1,0)) as approved_count,
+sum(state = "approved") as approved_count,
 sum(amount) as trans_total_amount,
 sum(if(state = "approved", amount, 0)) as approved_total_amount
 from Transactions
