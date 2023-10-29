a1c98132b67f965b694d9b297bda6cef6f71efbe
youmin <33755241+yuminee@users.noreply.github.com>
2023-10-29 16:07:21 +0900

    Time: 67 ms (57.39%), Space: 17.8 MB (10.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
+# class Solution:
+# 	def twoSum(self, nums: List[int], target: int) -> List[int]:
+# 		for idx1 in range(len(nums)):
+# 			num_to_find = target - nums[idx1]
+# 			for idx2 in range(idx1+1, len(nums)):
+# 				if num_to_find == nums[idx2]:
+# 					return [idx1, idx2]
+# 		return None
 class Solution:
 	def twoSum(self, nums: List[int], target: int) -> List[int]:
-		for idx1 in range(len(nums)):
-			num_to_find = target - nums[idx1]
-			for idx2 in range(idx1+1, len(nums)):
-				if num_to_find == nums[idx2]:
-					return [idx1, idx2]
+		num_to_find = {}
+		for idx in range(len(nums)):
+			if nums[idx] in num_to_find:
+				return [idx, num_to_find[nums[idx]]]
+			num_to_find[target - nums[idx]] = idx
 		return None
