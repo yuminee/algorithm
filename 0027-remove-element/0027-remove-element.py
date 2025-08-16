5145277fbc2fc8087d0a2ae4c6c04c325b523772
youmin <33755241+yuminee@users.noreply.github.com>
2025-08-16 17:15:30 +0900

    Time: 0 ms (100%), Space: 17.9 MB (10.98%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,17 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:  
-        run = True
-        break_point = 0
-        
-        while run:
-            for i in range(break_point, len(nums)):
-                if nums[i] == val:
-                    nums.pop(i)
-                    break_point = i
-                    break
-                elif i+1 == len(nums):
-                    run = False
-            if len(nums) == 0 or break_point == len(nums):
-                run = False
+        k = 0
 
-        return len(nums)
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
