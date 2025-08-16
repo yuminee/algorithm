39579cf1bfe00a22a5b3154999f46b632ad5e60f
youmin <33755241+yuminee@users.noreply.github.com>
2025-08-16 22:56:01 +0900

    Time: 4 ms (25.7%), Space: 18.8 MB (74.33%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,7 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = list(dict.fromkeys(nums))
-        for i in range(0, len(k)):
-            nums[i] = k[i]
+        # k = list(dict.fromkeys(nums))
+        # for i in range(0, len(k)):
+        #     nums[i] = k[i]
         
-        return len(k)
+        # return len(k)
+        i = 1
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i-1]: 
+                # 같지 않을때만 값을 덮어씌운다. 오름차순으로 정렬되어있기 때문에 가능하다.
+                nums[i] = nums[j]
+                i += 1
+        
+        return i
