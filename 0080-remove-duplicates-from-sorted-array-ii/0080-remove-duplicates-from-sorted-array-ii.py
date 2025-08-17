e42f99cfc2ee6ed05b62b8ee028998306238ce41
youmin <33755241+yuminee@users.noreply.github.com>
2025-08-17 12:26:12 +0900

    Time: 86 ms (49.98%), Space: 20.4 MB (79.34%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # nums는 오름차순으로 정렬되어있기 때문에 k-2와 i가 같은지를 비교해서 업데이트만 해도 된다.
         k = 2
 
         for i in range(2, len(nums)):
